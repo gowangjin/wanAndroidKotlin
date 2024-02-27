fe5773218c2b583864554325cd9eeb6c02eba98f
王进(Wang Jin,SA0)(东风日产乘用车公司技术中心新技术分院新技术分院（深圳）创新技术项目科)(8700394) <df-wangjin@dfl.com.cn>
2024-02-27 20:32:45 +0800

    [msg]1.引入ViewPager2 [module] App [author ]wangjin [type]change [taskId ] [bugId ] [checker]wangjin

--- app/src/main/java/com/example/wanandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/wanandroidkotlin/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.wanandroidkotlin
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import com.example.commonlibary.base.BaseActivity
 import com.example.commonlibary.base.BaseFragment
+import com.example.commonlibary.util.LogUtil
 import com.example.module_home.ui.HomeFragment
 import com.example.wanandroidkotlin.adapter.MainViewPagerAdapter
 import com.example.wanandroidkotlin.databinding.MainActivityBinding
@@ -14,26 +16,25 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<MainActivityBinding,MainActivityViewModel>() {
+    companion object{
+        private const val TAG = "MainActivity"
+    }
     private lateinit var mMainViewPagerAdapter: MainViewPagerAdapter
+    @Inject
+    public lateinit var mHomeFragment: HomeFragment
     override fun getLayoutId(): Int {
         return R.layout.main_activity
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
-
-    override fun providerVMClass(): Class<MainActivityViewModel> {
-        return MainActivityViewModel::class.java
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+        LogUtil.d(TAG, "onCreate: ")
         val fragmentList = arrayListOf<Fragment>()
-        fragmentList.add(HomeFragment())
+        fragmentList.add(mHomeFragment)
         mMainViewPagerAdapter = MainViewPagerAdapter(this,fragmentList)
         mBinding.viewPager.adapter = mMainViewPagerAdapter
         mBinding.bottomNavi.setOnItemSelectedListener {
+            LogUtil.d(TAG,"setOnItemSelectedListener ${it.itemId}")
             when(it.itemId){
                 R.id.home -> mBinding.viewPager.currentItem = 0
 //                R.id.project -> mBinding.viewPager.currentItem = 1
@@ -42,5 +43,9 @@ class MainActivity : BaseActivity<MainActivityBinding,MainActivityViewModel>() {
             }
             true
         }
+    }
+
+    override fun providerVMClass(): Class<MainActivityViewModel> {
+        return MainActivityViewModel::class.java
     }
 }
--- module_home/src/main/java/com/example/module_home/ui/HomeFragment.kt
+++ module_home/src/main/java/com/example/module_home/ui/HomeFragment.kt
@@ -1,13 +1,16 @@
 package com.example.module_home.ui
 
 import android.os.Bundle
+import android.view.View
 import com.example.commonlibary.base.BaseFragment
 import com.example.module_home.R
 import com.example.module_home.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 import logic.model.HomeViewModel
+import javax.inject.Inject
+
 @AndroidEntryPoint
-class HomeFragment :BaseFragment<FragmentHomeBinding, HomeViewModel>(){
+class HomeFragment @Inject constructor() :BaseFragment<FragmentHomeBinding, HomeViewModel>(){
     override fun getLayoutId(): Int {
         return R.layout.fragment_home
     }
@@ -18,6 +21,10 @@ class HomeFragment :BaseFragment<FragmentHomeBinding, HomeViewModel>(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mViewModel.requestBanner()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+       // mViewModel.requestBanner()
     }
 }
